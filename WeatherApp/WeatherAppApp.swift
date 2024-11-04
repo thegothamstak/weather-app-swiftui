c9b9aa1cfa3a83968296d4d53be7ed3847facd3c
stak_rxconnect <stak@rxconnect.ca>
2024-11-04 10:21:47 -0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Shripad Tak on 2024-11-04.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
